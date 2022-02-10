37e245db84352dd42944bac040a8345cf2d9a097
Thimoty Barbieri <thimoty@thimoty.it>
2022-02-10 18:30:04 +0100

    aggiunti secret e service principal

--- MessageContracts/manifests-k8s/az-k8s-clusterr-create.sh
+++ MessageContracts/manifests-k8s/az-k8s-clusterr-create.sh
@@ -4,7 +4,9 @@ ACR_NAME=myacrtimo
 az aks create --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --node-count 1 --enable-rbac --enable-addons monitoring --enable-addons http_application_routing --generate-ssh-keys
 #az aks install-cli
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
-az aks update -n $CLUSTER_NAME -g $RESOURCE_GROUP --attach-acr $ACR_NAME
+# Se si è Owner o Admin si può registrare il container registry sul cluster,
+# altrimenti creare un service principal, ottenere pw e id, creare un secret, e usare PullSecrets nei pod
+# az aks update -n $CLUSTER_NAME -g $RESOURCE_GROUP --attach-acr $ACR_NAME
 ##
 ##
 az aks delete --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME 
---
+++ MessageContracts/manifests-k8s/service-principal-create.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script requires Azure CLI version 2.25.0 or later. Check version with `az --version`.
+# reference: https://docs.microsoft.com/en-us/azure/container-registry/container-registry-auth-kubernetes
+
+# Modify for your environment.
+# ACR_NAME: The name of your Azure Container Registry
+# SERVICE_PRINCIPAL_NAME: Must be unique within your AD tenant
+ACR_NAME=myacrtimo
+SERVICE_PRINCIPAL_NAME=acr-service-principal
+
+# Obtain the full registry ID for subsequent command args
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query "id" --output tsv)
+
+# Create the service principal with rights scoped to the registry.
+# Default permissions are for docker pull access. Modify the '--role'
+# argument value as desired:
+# acrpull:     pull only
+# acrpush:     push and pull
+# owner:       push, pull, and assign roles
+PASSWORD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query "password" --output tsv)
+USER_NAME=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv)
+
+# Output the service principal's credentials; use these in your services and
+# applications to authenticate to the container registry.
+echo "Service principal ID: $USER_NAME"
+echo "Service principal password: $PASSWORD"
+
+# Create secret
+kubectl create secret docker-registry acr-secret \
+    --namespace default \
+    --docker-server=$ACR_NAME.azurecr.io \
+    --docker-username=$USER_NAME \
+    --docker-password=$PASSWORD
